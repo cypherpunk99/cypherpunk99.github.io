4042c98cc23df4cd11afe49140039f28f52567ce
Alex Matveev <amatveevdev9@gmail.com>
2020-09-26 16:06:06 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Hero from './Hero/Hero';
 // import About from './About/About';
 // import Projects from './Projects/Projects';
 // import Contact from './Contact/Contact';
 // import Footer from './Footer/Footer';
 
-// import { PortfolioProvider } from '../context/context';
+import { PortfolioProvider } from '../context/context';
 
-// import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
+import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
 
 function App() {
-  // const [hero, setHero] = useState({});
-  // const [about, setAbout] = useState({});
-  // const [projects, setProjects] = useState([]);
-  // const [contact, setContact] = useState({});
-  // const [footer, setFooter] = useState({});
+  const [hero, setHero] = useState({});
+  const [about, setAbout] = useState({});
+  const [projects, setProjects] = useState([]);
+  const [contact, setContact] = useState({});
+  const [footer, setFooter] = useState({});
 
-  // useEffect(() => {
-  //   // setHero({ ...heroData });
-  //   // setAbout({ ...aboutData });
-  //   // setProjects([...projectsData]);
-  //   // setContact({ ...contactData });
-  //   // setFooter({ ...footerData });
-  // }, []);
+  useEffect(() => {
+    setHero({ ...heroData });
+    setAbout({ ...aboutData });
+    setProjects([...projectsData]);
+    setContact({ ...contactData });
+    setFooter({ ...footerData });
+  }, []);
 
-  return <Hero />;
+  return (
+    <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
+      <Hero />
+      {/* <About /> */}
+      {/* <Projects /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
+    </PortfolioProvider>
+  );
 }
 
 export default App;
