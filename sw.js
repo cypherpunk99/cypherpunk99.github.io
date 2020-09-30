a095b99717ed3db9f7bde8fd14902d0b8e33c596
Alex Matveev <amatveevdev9@gmail.com>
2020-09-30 14:48:06 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-616186ae7e33b09dbe61.js"
+    "url": "webpack-runtime-d8eeffd02201f1604435.js"
   },
   {
     "url": "framework-868c53fff4771b0bde98.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b738e1f5db60fbac94792466598bc983"
+    "revision": "0bd04b4848b2ed3143c4f851b16e9ac6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
